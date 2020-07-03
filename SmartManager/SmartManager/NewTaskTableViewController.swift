d58eb80023aeac85819ad5914a8de9f4712e3333
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-03 22:53:02 +0300

    added ability to choose image or to take photo, and edit chosen image

--- SmartManager/SmartManager/NewTaskTableViewController.swift
+++ SmartManager/SmartManager/NewTaskTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewTaskTableViewController: UITableViewController {
 
+    @IBOutlet weak var taskImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +22,31 @@ class NewTaskTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) {_ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
@@ -35,5 +62,27 @@ extension NewTaskTableViewController: UISearchTextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: Work with image
+extension NewTaskTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        taskImage.image = info[.editedImage] as? UIImage
+        taskImage.contentMode = .scaleAspectFill
+        taskImage.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
