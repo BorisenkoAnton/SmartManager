97209b1f95e97fc24dd62ae64af53feb1f33b1ed
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-07 12:47:37 +0300

    added Stars frame

--- SmartManager/SmartManager/MainScreenTableViewCell.swift
+++ SmartManager/SmartManager/MainScreenTableViewCell.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import Cosmos
 
 class MainScreenTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var taskImage: UIImageView!
+    @IBOutlet weak var taskImage: UIImageView! {
+        didSet {
+            // to make image view to be circle
+            taskImage.layer.cornerRadius = taskImage.frame.size.height / 2
+            taskImage.clipsToBounds = true
+        }
+    }
     @IBOutlet weak var taskNameLabel: UILabel!
     @IBOutlet weak var taskLocationLabel: UILabel!
     @IBOutlet weak var taskDateLabel: UILabel!
-    
+    @IBOutlet weak var cosmosView: CosmosView! {
+        didSet {
+            cosmosView.settings.updateOnTouch = false
+        }
+    }
     
 }
